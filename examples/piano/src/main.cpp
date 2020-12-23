d1e664270213190f92c2d7923eb4b2501be61cea
Vikram Singh <vikzilla18@gmail.com>
2020-12-23 15:49:37 -0500

    Cleaned up the piano example

--- examples/piano/src/main.cpp
+++ examples/piano/src/main.cpp
@@ -5,51 +5,38 @@
 #include <VSynth/Synthesizer.h>
 #include <VSynth/Waveforms.h>
 
-#include <cmath>
-
 using namespace VSynth;
-
-ADSREnvelope pianoADSR(0.90f, 0.60f, 0.20f, 0.20f, 0.50f);
 
 struct PianoKey
 {
     char key;
     double frequency;
-    Envelope env;
 };
 
 std::vector<PianoKey> pianoKeys = {
-    {SDLK_a, 262, Envelope(pianoADSR)},
-    {SDLK_s, 294, Envelope(pianoADSR)},
-    {SDLK_d, 330, Envelope(pianoADSR)},
-    {SDLK_f, 350, Envelope(pianoADSR)},
-    {SDLK_g, 392, Envelope(pianoADSR)},
-    {SDLK_h, 440, Envelope(pianoADSR)},
-    {SDLK_j, 494, Envelope(pianoADSR)},
-    {SDLK_k, 523, Envelope(pianoADSR)}
-};
+    {SDLK_a, 262},
+    {SDLK_s, 294},
+    {SDLK_d, 330},
+    {SDLK_f, 350},
+    {SDLK_g, 392},
+    {SDLK_h, 440},
+    {SDLK_j, 494},
+    {SDLK_k, 523}};
 
-Instrument *wave = nullptr;
-
-void createInstruments(Synthesizer &synth)
+Instrument *createInstrument()
 {
-
-    wave = new Instrument(
-        [](double freq, double time){
-            return 0.5 * (
-                Waveforms::sine(freq, time) +
-                0.5 * Waveforms::sine(freq*2, time) +
-                0.25 * Waveforms::sine(freq*3, time) +
-                0.125 * Waveforms::sine(freq*4, time) +
-                0.0625 * Waveforms::sine(freq*5, time) +
-                0.03125 * Waveforms::sine(freq*6, time) +
-                0.015625 * Waveforms::sine(freq*7, time)
-            );
+    return new Instrument(
+        [](double freq, double time) {
+            return 0.5 * (Waveforms::sine(freq, time) +
+                          0.5 * Waveforms::sine(freq * 2, time) +
+                          0.25 * Waveforms::sine(freq * 3, time) +
+                          0.125 * Waveforms::sine(freq * 4, time) +
+                          0.0625 * Waveforms::sine(freq * 5, time) +
+                          0.03125 * Waveforms::sine(freq * 6, time) +
+                          0.015625 * Waveforms::sine(freq * 7, time));
         },
-        pianoADSR
-    );
-
-    synth.addInstrument(wave);
+        ADSREnvelope(0.90f, 0.60f, 0.20f, 0.20f, 0.50f)
+        );
 }
 
 int main(int argc, char *argv[])
@@ -59,9 +46,11 @@ int main(int argc, char *argv[])
     SDL_Window *window;
     window = SDL_CreateWindow("SDL_Test_Windows", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1600, 900, SDL_WINDOW_SHOWN);
 
+    Instrument *piano = createInstrument();
+
     Synthesizer synth;
     synth.open();
-    createInstruments(synth);
+    synth.addInstrument(piano);
     synth.unpause();
 
     bool running = true;
@@ -82,13 +71,11 @@ int main(int argc, char *argv[])
                     {
                         if (e.type == SDL_KEYDOWN)
                         {
-                            // it->env.hold();
-                            wave->holdNote(it->frequency);
+                            piano->holdNote(it->frequency);
                         }
                         else if (e.type == SDL_KEYUP)
                         {
-                            // it->env.release();
-                            wave->releaseNote(it->frequency);
+                            piano->releaseNote(it->frequency);
                         }
                     }
                 }
