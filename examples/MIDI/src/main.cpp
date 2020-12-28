9534e62b2cc02e822be3bd786fa702818c33ccca
Vikram Singh <vikzilla18@gmail.com>
2020-12-28 16:53:55 -0500

    Updated midi sequencer to sort notes. Also modified the patch for the piano to make it less computationally expensive. Added Midifile license #18

--- examples/MIDI/src/main.cpp
+++ examples/MIDI/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <SDL2/SDL.h>
 
+#include <VSynth/Generators/MonophonicInstrument.h>
 #include <VSynth/Generators/PolyphonicInstrument.h>
 #include <VSynth/Generators/Sequencer.h>
 #include <VSynth/Synthesizer.h>
@@ -11,8 +12,6 @@
 #include <algorithm>
 #include <vector>
 
-#include <iostream>
-
 double noteToFrequency(int midiNote)
 {
     return 440.0 * pow(2.0, ((double)midiNote - 69.0) / 12.0);
@@ -21,36 +20,20 @@ double noteToFrequency(int midiNote)
 void addNotesToSequencer(std::vector<VSynth::Sequencer> &seqs, smf::MidiFile &file)
 {
     int tracks = file.getTrackCount();
-    std::cout << "Number of Tracks: " << tracks << std::endl;
-
-    std::cout << "Ticks Per Quarter Note: " << file.getTicksPerQuarterNote() << std::endl;
 
     for (int track = 0; track < tracks; track++)
     {
-        std::cout << "Track " << track << std::endl;
-        std::cout << "Seconds\tMessage" << std::endl;
         for (int event = 0; event < file[track].size(); event++)
         {
             if (file[track][event].isNoteOn())
             {
-                // std::cout << "Start Time: " << file[track][event].seconds << "\t";
-                // std::cout << "Duration: " << file[track][event].getDurationInSeconds() << "\t";
-                // std::cout << "Note: " << std::hex << file[track][event].getP1() << std::dec << std::endl;
-
                 double note = noteToFrequency(file[track][event].getP1());
                 double startTime = file[track][event].seconds;
                 double duration = file[track][event].getDurationInSeconds();
 
                 seqs[file[track][event].getChannelNibble()].queueNote(note, startTime, duration);
-                // std::cout << std::dec << file[track][event].seconds;
-                // std::cout << '\t' << file[track][event].getDurationInSeconds();
-                // std::cout << '\t' << std::hex;
-                // for (int i = 0; i < file[track][event].size(); i++)
-                //     std::cout << (int)file[track][event][i] << ' ';
-                // std::cout << std::endl;
             }
         }
-        std::cout << std::endl;
     }
 }
 
@@ -71,11 +54,7 @@ int main(int argc, char *argv[])
     window = SDL_CreateWindow("MIDI", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1600, 900, SDL_WINDOW_SHOWN);
 
     // Create an instrument with the following envelope and patch
-    VSynth::ADSREnvelope pianoEnvelope(0.90f, 0.30f, 0.01f, 0.01f, 0.01f);
-    VSynth::Instrument *piano = new VSynth::PolyphonicInstrument(
-        VSynth::Patches::PIANO,
-        pianoEnvelope);
-
+    VSynth::ADSREnvelope pianoEnvelope(0.90f, 0.30f, 0.1f, 0.1f, 0.50f);
     std::vector<VSynth::Sequencer> seqs;
 
     for (int i = 0; i < 16; i++)
@@ -84,15 +63,15 @@ int main(int argc, char *argv[])
         VSynth::Sequencer seq(new VSynth::PolyphonicInstrument(
             VSynth::Patches::PIANO,
             pianoEnvelope));
-        seq.setVolume(50);
         seqs.push_back(seq);
     }
 
     addNotesToSequencer(seqs, midifile);
 
-    VSynth::Synthesizer synth(30000, 2);
+    VSynth::Synthesizer synth(25000, 50);
     synth.open();
     for(auto it = seqs.begin(); it != seqs.end(); it++){
+        it->sortNotes();
         synth.addSoundGenerator(&(*it));
     }
     synth.unpause();
