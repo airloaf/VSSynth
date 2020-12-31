14fb00ef3cc4a045ebf9e234441b4d5559f05a5a
Vikram Singh <vikzilla18@gmail.com>
2020-12-31 12:11:34 -0500

    Updated README with new build instruction and updated example table. #28

--- examples/MIDI/README.md
+++ examples/MIDI/README.md
@@ -1,11 +1,17 @@
 # MIDI
 
+A MIDI player using VSynth to synthesize instruments. You can consider this to be the "Capstone" of the VSynth examples.
+
 ## Dependencies
 
-- SDL2
-- VSynth
-- Midifile
+Since this example shows how VSynth handles playing MIDI files and not how to parse MIDI files, I have used the following library to parse the MIDI files.
+
+- [Midifile](https://github.com/craigsapp/midifile)
+
+## What you will learn
+
+- Creating new sound generators
 
 ## References
 
-- [midifile library](https://github.com/craigsapp/midifile)
+- [Midifile API](https://midifile.sapp.org/)
--- examples/SimpleTone/src/main.cpp
+++ examples/SimpleTone/src/main.cpp
@@ -10,19 +10,26 @@ using namespace VSynth;
 int main(int argc, char *argv[])
 {
     // SDL initialization
+    // SDL has to be initialized with SDL_INIT_AUDIO otherwise the synthesizer will not work!
     SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO);
     SDL_Window *window;
     window = SDL_CreateWindow("Simple Tone Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 450, SDL_WINDOW_SHOWN);
 
-    // Create a Sine wave at "Middle C"
+    // Creates a Sine wave at "Middle C"
+    // In this example we create the sine wave in-line,
+    // there are synth patches available with the library
+    // that produce richer noises than just a sine wave.
+    // See the Patches.h file within utils
     Tone tone(
         [](double frequency, double time) {
             return Waveforms::sine(frequency, time);
         });
     tone.playNote(Notes::C4);
 
-    // Creating a synthesizer
+    // Create a synthesizer, with default settings
     Synthesizer synth;
+
+    // Open the synth for playback with the sine wave we have created
     synth.open();
     synth.addSoundGenerator(&tone);
     synth.unpause();
@@ -40,9 +47,13 @@ int main(int argc, char *argv[])
             }
         }
     }
+
+    // Prior to destroying the synth, it is good
+    // practice to pause the synth first!
     synth.pause();
     synth.close();
 
+    // SDL de-initialization
     SDL_DestroyWindow(window);
     SDL_Quit();
 
