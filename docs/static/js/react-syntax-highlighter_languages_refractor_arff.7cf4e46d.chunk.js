d7e371c1d79286523e02e7b155d33bb92dfcd111
Vikram Singh <vikzilla18@gmail.com>
2021-01-06 16:34:52 -0500

    Moved web page build to docs folder

---
+++ docs/static/js/react-syntax-highlighter_languages_refractor_arff.7cf4e46d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonplanding-page"]=this["webpackJsonplanding-page"]||[]).push([[8],{161:function(a,n,e){"use strict";function t(a){a.languages.arff={comment:/%.*/,string:{pattern:/(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,greedy:!0},keyword:/@(?:attribute|data|end|relation)\b/i,number:/\b\d+(?:\.\d+)?\b/,punctuation:/[{},]/}}a.exports=t,t.displayName="arff",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_arff.7cf4e46d.chunk.js.map
